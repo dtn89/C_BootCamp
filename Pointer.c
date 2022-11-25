7211f8038dac45561b0ad71cf98e4fdefeb820a3
TrungNam <dangtrungnam2809@gmail.com>
2022-11-25 10:17:54 +0700

    Update Pointer source code

--- Pointer.c
+++ Pointer.c
@@ -9,7 +9,7 @@ int main()
     char a = 'A';
     char *ptr = &a;
 
-    printf("1 | %c, %c, %llu\n", a, *ptr, (uint64_t) ptr);
+    printf("1 | %c, %c, %llu\n", a, *ptr, (uint64_t)ptr);
 
     char b = 'B';
     ptr = &b;
@@ -25,6 +25,27 @@ int main()
     unsigned x = 'A';
     ptr = (char *)&x;
     printf("5 | %u, %c, %llu\n", x, *ptr, (uint64_t) &x);
+
+    unsigned y = 'B';
+    ptr = (char *)&y;
+    printf("6 | %u, %c, %llu\n", y, *ptr, (uint64_t) &y);
+
+    *ptr = 'C';
+    printf("7 | %u, %c, %llu\n", y, *ptr, (uint64_t) &y);
+
+    (*ptr)++;
+    printf("8 | %u, %c, %llu\n", y, *ptr, (uint64_t) &y);
+
+    x = 0xABCD1234;
+    ptr = (char *)&x;
+    printf("9 | %X, %X, %c\n", x, *ptr, *ptr);
+
+    *ptr = 'C';
+    printf("10 | %X, %X, %c\n", x, *ptr, *ptr);
+
+    unsigned *uint_ptr = (unsigned *)ptr;
+    *uint_ptr = 'C';
+    printf("11 | %u, %X, %X, %c\n",x , x, *ptr, *ptr);
 
     return EXIT_SUCCESS;
 }
