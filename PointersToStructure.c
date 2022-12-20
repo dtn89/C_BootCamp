d178f2cda5359a276c7695e9d43f4bffe89cf44b
TrungNam <dangtrungnam2809@gmail.com>
2022-12-21 03:37:58 +0700

    Adding Pointers to Structure source code

---
+++ PointersToStructure.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_DIMENSIONS 3
+
+typedef struct Guy
+{
+    char *name;
+    bool active;
+    int dimensions;
+    double pos[MAX_DIMENSIONS];
+    struct Guy *friend;
+}Guy;
+
+int main()
+{
+    printf("\n=== Pointers to Structures ===\n");
+
+    Guy bill = {
+        .name = "Bill Gates",
+        .pos = {3, 2, 2},
+        .dimensions = 3,
+        .active = true,
+    };
+
+    Guy *firstGuy = &bill;
+
+    /* 1) Accessing the struct directly */
+    printf("Guy's name: %s\n", bill.name);
+
+    /* 2) Accessing the struct through pointers using the indirection operator */
+    printf("Guy's name: %s\n", (*firstGuy).name);
+
+    /* 3) Accessing the struct through pointers using the arrow operator */
+    printf("Guy's name: %s\n", firstGuy->name);
+    printf("Guy's position %.1f, %.1f, %.1f\n",
+            firstGuy->pos[0],
+            firstGuy->pos[1],
+            firstGuy->pos[2]);
+    
+    /* Walking a single linked list... */
+    firstGuy->friend = malloc(sizeof(Guy));
+    firstGuy->friend->name = "Elon Musk";
+    firstGuy->friend->active = false;
+
+
+    /* Go through the list... */
+    firstGuy->friend->friend = malloc(sizeof(Guy));
+    firstGuy->friend->friend->name = "Andrea";
+
+    /* Allocating an array of guys */
+    int num = 10;
+    Guy *manyGuys = malloc(num * sizeof(Guy));
+
+    manyGuys[0].name = "Mario";
+    manyGuys[0].friend = &bill;
+
+    /* Accessing the 2nd element of manyGuys using pointers */
+    (manyGuys + 1)->name = "Tom";
+    printf("manyGuy[1].name = %s\n", manyGuys[1].name);
+
+    printf("\n\n=== TrungNam Dang ===\n\n");
+    return EXIT_SUCCESS;
+}
